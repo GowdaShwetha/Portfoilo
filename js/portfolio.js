819f92a64d92e1f68cf87093c3b2245d63648fd1
GowdaShwetha <shwethagowda252001@gmail.com>
2024-02-29 13:06:25 +0530

    commit

---
+++ js/portfolio.js
@@ -0,0 +1,14 @@
+function resume(){
+    window.location="./resume.html";
+}
+
+
+
+//  setting margin 
+
+const a=document.getElementsByName('about');
+function about()
+{
+a.style.paddingTop='150%';
+
+}
